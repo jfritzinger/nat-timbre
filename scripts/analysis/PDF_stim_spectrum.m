2ffa8c9049bbb527db3eddcc24297ed050d6d9f4
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-02-14 13:19:54 -0500

    No major changes to code

--- scripts/analysis/PDF_stim_spectrum.m
+++ scripts/analysis/PDF_stim_spectrum.m
@@ -2,54 +2,33 @@
 % J. Fritzinger, 7/19/2021
 
 %% Load in data
-clear all 
 
-instrument = 'Bass';
-fpath = 'C:\Users\jfritzinger\Box\02 - Physiology\07 - Natural Timbre\Cut Waveforms\';
-files = dir([fpath instrument '.*']);
-%files = dir([fpath '*.wav']);
-
-% Load in tuning sheet
-tuning_dir = 'C:\Users\jfritzinger\Box\02 - Physiology\07 - Natural Timbre\Cut Waveforms\';
-tuning = readtable([tuning_dir 'Tuning.xlsx']);
-
-% Sort timbre
-k = [];
-index = [];
-for ii = 1:size(files, 1)
-    for iii = 1:size(tuning)
-        k = strfind(files(ii).name, tuning.Note{iii});
-        if ~isnan(k)
-            index(ii) = iii;
-        end
-    end
+if ismac
+	fpath = '/Users/jfritzinger/Library/CloudStorage/Box-Box/02 - Code/Nat-Timbre/data';
+else
+	fpath = 'C:\Users\jfritzinger\Box\02 - Code\Nat-Timbre\data\';
 end
-pitch_order = tuning.Frequency(index);
-[pitch_order, order] = sort(pitch_order);
+tuning = readtable(fullfile(fpath, 'Tuning.xlsx')); % Load in tuning 
+target = 'Bassoon';
 
+% Get all .wav files containing the target instrument name
+listing = dir(fullfile(fpath, 'waveforms', ['*' target '*.wav']));
+files = {listing.name};
+
+% Extract note names and find corresponding frequencies
+note_names = extractBetween(files, 'ff.', '.');
+[~, index] = ismember(note_names, tuning.Note);
+F0s = tuning.Frequency(index);
+
+% Sort files and frequencies by pitch
+[F0s, order] = sort(F0s);
 files = files(order);
-for ii = 1:size(files, 1)
-    [y(ii,:), Fs] = audioread([fpath files(ii).name]);
+
+% Load in stimulus
+for ii = 1:size(files, 2)
+    [y(ii,:), Fs] = audioread(fullfile(fpath,'waveforms', files{ii}));
 end
 t = linspace(0, length(y)/Fs, length(y));
-
-
-
-%% Calculate spectra
-
-for ii = 1:size(files, 1)
-    y2 = fft(y(ii, :));
-    m = abs(y2);
-    mdB = 20*log10(m);
-    f = (0:length(y2)-1)*Fs/length(y2);
-    mdB(mdB<0) = 0;
-    f(f>Fs/2) = [];
-    mdB = mdB(1:length(f));
-    log_f = log10(f(:,2:end));
-    center_of_mass(ii) = 10.^((sum(log_f.*mdB(:,2:end), 2))./sum(mdB(:,2:end), 2));
-    mdB_all(ii,:) = mdB;
-    f_all(ii, :) = f;
-end
 
 %% Set up PDF report
 import mlreportgen.dom.*
@@ -57,7 +36,7 @@ import mlreportgen.report.*
 
 % Initialize report
 images = {}; %hold all plots as images, need to delete when finished
-report_name = [extractBefore(files(1).name, '.') '.pdf'];
+report_name = 'Bassoon.pdf';
 rpt = Document(report_name, 'pdf'); %initialize report document as pdf
 open(rpt);
 pm = rpt.CurrentPageLayout;
@@ -72,24 +51,53 @@ pm.PageMargins.Right = '0.2in';
 
 %% Plot data in PDF format
 
-% % Generate page heading
-% h = Heading(2, sprintf('%s', extractBefore(files(1).name, '.')));
-% b = Border();
-% b.BottomStyle = 'single';
-% b.BottomColor = 'LightGray';
-% b.BottomWidth = '1pt';
-% h.Style = [h.Style {Color('Black'), b}, {PageBreakBefore()}];
-% append(rpt,h);
+for ii = 25 %1:size(files, 1)
 
-for pdf_ind = 1:size(files, 1)
+	y2 = fft(y(ii, :));
+    m = abs(y2);
+    mdB = 20*log10(m);
+    f = (0:length(y2)-1)*Fs/length(y2);
+    mdB(mdB<0) = 0;
+    f(f>Fs/2) = [];
+    mdB = mdB(1:length(f));
+    log_f = log10(f(:,2:end));
+    center_of_mass(ii) = 10.^((sum(log_f.*mdB(:,2:end), 2))./sum(mdB(:,2:end), 2));
+    mdB_all(ii,:) = mdB;
+    f_all(ii, :) = f;
+
+	% % Generate page heading
+	% h = Heading(2, sprintf('%s', extractBefore(files(1).name, '.')));
+	% b = Border();
+	% b.BottomStyle = 'single';
+	% b.BottomColor = 'LightGray';
+	% b.BottomWidth = '1pt';
+	% h.Style = [h.Style {Color('Black'), b}, {PageBreakBefore()}];
+	% append(rpt,h);
      
     % Adds spectra 
-    note = extractBetween(files(pdf_ind).name,'ff.','.wav');
-    %string = extractBetween(files(pdf_ind).name,'ff.','.');
-    name = [extractBefore(files(pdf_ind).name, '.') ', ' note{1} ', ' num2str(pitch_order(pdf_ind)) 'Hz'];
-    [ssplt, images] = pdfStimulusSpectra(f_all(pdf_ind, :), mdB_all(pdf_ind, :),...
-        center_of_mass(pdf_ind), pdf_ind, name, images);
-    append(rpt, ssplt);
+    note = extractBetween(files{ii},'ff.','.wav');
+    name = [extractBefore(files{ii}, '.') ', ' note{1} ', ' num2str(F0s(ii)) 'Hz'];
+    [ssplt, images] = pdfStimulusSpectra(f_all(ii, :), mdB_all(ii, :),...
+        center_of_mass(ii), ii, name, images);
+	append(rpt, ssplt);
+
+	% Spectrogram
+	npts = round(0.003 * Fs); % 3ms 
+	window = gausswin(npts);
+	noverlap = round(npts * 0.9);
+	[s, w, t] = spectrogram(y(ii,:), window, noverlap, [],Fs,'yaxis');
+
+
+	figure
+	spec = 20*log10(abs(s));
+	pcolor(t, w, spec);
+	shading interp
+	set(gca, 'yscale', 'log')
+	colormap(gray)
+	colorbar
+	yticks([100 200 500 1000 5000 10000])
+
+	
 end
 
 close(rpt);
