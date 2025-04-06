16a15a50f41a4e27e1c8099ede1ae0adc4e47c4d
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-04-06 09:26:48 -0400

    Added temproal analysis

--- scripts/analysis/PDF_all_units.m
+++ scripts/analysis/PDF_all_units.m
@@ -11,7 +11,7 @@ sessions = readtable(fullfile(datapath, 'data-cleaning', spreadsheet_name), 'Pre
 num_data = size(sessions, 1);
 
 % Initialize report
-filename = 'NT_RVF';
+filename = 'NT_Oboe';
 images = {}; %hold all plots as images, need to delete when finished
 datetime.setDefaultFormats('default','yyyy-MM-dd_hhmmss')
 report_name = sprintf('%s/pdfs/%s_%s.pdf', savepath, datetime, filename);
@@ -27,12 +27,14 @@ pm.PageMargins.Footer = '0.01in';
 pm.PageMargins.Left = '0.2in';
 pm.PageMargins.Right = '0.2in';
 
+target = 13; % 13 - oboe, 14 - bassoon
+
 %% Plot each dataset 
 
 % Find sessions for target synthetic timbre response
+%bin200(:,1) = cellfun(@(s) contains(s, 'R'), sessions.Oboe);
 bin200(:,1) = cellfun(@(s) contains(s, 'R'), sessions.Oboe);
-bin200(:,2) = cellfun(@(s) contains(s, 'R'), sessions.Bassoon);
-has_data = bin200(:,1) | bin200(:,2);
+has_data = bin200(:,1); % | bin200(:,2);
 index = find(has_data);
 
 % Sort by CF
@@ -42,6 +44,9 @@ num_sessions = length(CF_list);
 
 % Plot each neuron
 for isesh = 1:num_sessions
+	% if isesh == 27 || isesh == 181 || isesh == 185
+	% 	continue
+	% end
 	ineuron = index(order(isesh)); %indices(isesh)
 	if any(has_data(ineuron))
 
@@ -63,13 +68,14 @@ for isesh = 1:num_sessions
 
 		% Set up figure
 		fig = figure('Position',[292,274,1264,420]);
-		tiledlayout(2, 6, 'TileSpacing','compact', 'Padding','tight')
+		tiledlayout(4, 3, 'TileSpacing','compact', 'Padding','tight', ...
+			'TileIndexing','columnmajor')
 		x_label = [1000 2000 3000 4000 6000 8000]./1000;
 		fontsize = 10;
 
 		% Plot RM
 		params_RM = data{2, 2};
-		nexttile([1, 2])
+		nexttile()
 		if ~isempty(params_RM)
 			data_RM = analyzeRM(params_RM);
 			hold on
@@ -98,7 +104,7 @@ for isesh = 1:num_sessions
 
 		% Plot MTF
 		params_MTF = data{3, 2};
-		nexttile([1, 2])
+		nexttile()
 		if ~isempty(params_RM)
 			data_MTF = analyzeMTF(params_MTF);
 			hold on
@@ -119,26 +125,100 @@ for isesh = 1:num_sessions
 
 		% Plot RVF
 		params_RVF = data{5,1};
-		nexttile([1, 2])
+		nexttile()
 		if ~isempty(params_RVF)
 			plotPhysRVF([], params_RVF, []);
 		end
 
 		% Plot NT
-		params_NT = data(13:14, 2);
-		data_NT = cell(2, 1);
-		for ii = 1:2
-			nexttile([1, 3])
-			if ~isempty(params_NT{ii})
-				data_NT = analyzeNT(params_NT{ii});
-				bar(data_NT.pitch, data_NT.rate)
-				hold on
-				errorbar(data_NT.pitch,data_NT.rate, data_NT.rate_std/sqrt(params_NT{ii}.nrep), 'LineStyle', 'none', 'Color', [0 0 0])
-				xlabel('Pitch (Hz)')
-				title(extractBefore(params_NT{ii}.filename{1}, '.'))
-				hold off
-			end
+		nexttile()
+		params_NT = data(target, 2);
+		if ~isempty(params_NT{1})
+			yyaxis left
+			data_NT = analyzeNT(params_NT{1});
+			temporal = analyzeNT_Temporal(data_NT);
+			hold on
+			errorbar(data_NT.pitch_num,data_NT.rate, data_NT.rate_std/...
+				sqrt(params_NT{1}.nrep), 'LineWidth',1.5)
+			xlabel('F0 (Hz)')
+			ylabel('Avg. Rate (sp/s)')
+			xlim([55 600])
+			xticks([55 110 220 440 600])
+			title(extractBefore(params_NT{1}.filename{1}, '.'))
+			hold off
+			set(gca, 'xscale', 'log')
+
+			% Plot vector strength
+			yyaxis right
+			hold on
+			plot(data_NT.pitch_num, temporal.VS)
+			xlabel('F0 (Hz)')
+			ylabel('Vector Strength')
+			set(gca, 'fontsize', fontsize)
+			set(gca, 'xscale', 'log')
+			xlim([55 600])
+			xticks([55 110 220 440 600])
+			ylim([0 1])
+			grid on
 		end
+
+		% Plot PSTH
+		max_rate = max(temporal.PSTH, [], 'all');
+		note_values = round(data_NT.pitch_num);
+		nexttile([4, 1])
+		num_stim = length(note_values);
+		hold on
+		for j = 1:num_stim
+
+			% Plot PSTHs
+			counts = temporal.PSTH(j,:);
+			edges = temporal.t;
+			t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
+			x_patch = repelem(edges, 2);
+			y_patch = repelem([0; counts(:); 0]', 2);
+			y_patch = y_patch(2:end-1); % Creates [0 y1 y1 0 0 y2 y2 0...]
+			offset = (j-1)*max_rate; % Adjust offset amount
+			patch(x_patch, y_patch + offset, 'b', 'FaceAlpha',0.5, 'EdgeColor','k');
+
+			% Plot smoothed PSTH
+			plot(temporal.t(1:end-1), temporal.PSTH_smooth(j,:)+offset,'k', 'LineWidth',1.5);
+
+		end
+		ylim([0 max_rate*num_stim])
+		xlabel('Time (ms)')
+		box on
+		yticks(linspace(max_rate/2, max_rate*num_stim-max_rate/2, num_stim))
+		yticklabels(note_values)
+		title('PSTH')
+		set(gca, 'fontsize', fontsize)
+
+		% Plot period histogram
+		nexttile([4, 1])
+		max_rate = max(temporal.p_hist, [], 'all');
+		for j = 1:num_stim
+
+			% Plot PSTHs
+			counts = temporal.p_hist(j,:);
+			edges = temporal.t_hist(j,:);
+			t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
+			x_patch = repelem(edges, 2);
+			y_patch = repelem([0; counts(:); 0]', 2);
+			y_patch = y_patch(2:end-1); % Creates [0 y1 y1 0 0 y2 y2 0...]
+			offset = (j-1)*max_rate; % Adjust offset amount
+			patch(x_patch, y_patch + offset, 'b', 'FaceAlpha',0.5, 'EdgeColor','k');
+			period = 1/data_NT.pitch_num(j)*1000;
+		end
+		ylim([0 max_rate*num_stim])
+		xlabel('Time (ms)')
+		box on
+		yticks(linspace(max_rate/2, max_rate*num_stim-max_rate/2, num_stim))
+		yticklabels(note_values)
+		xlim([0 17.5])
+		%xticks(1:5)
+		grid on
+		title('Period Histogram')
+		set(gca, 'fontsize', fontsize)
+
 
 		% Add to PDF
 		[plt1, images] = addtoSTPDF(images, fig, putative);
@@ -160,7 +240,7 @@ function [img, images] = addtoSTPDF(images, fig, title)
 import mlreportgen.dom.*
 
 % Set figure size, recommended
-values = [8, 5];
+values = [7.75, 10.5];
 fig.PaperSize = values;
 fig.PaperPosition = [0 0 values];
 fig.Units = 'inches';
