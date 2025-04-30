ca97f2431cdd98b3b71971eada23820e76b2d98d
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-04-29 20:57:20 -0400

    Added analysis plots including splitting up F0 and finding the three best neurons

--- scripts/decoding-models/model_neuron_temporal_F0.m
+++ scripts/decoding-models/model_neuron_temporal_F0.m
@@ -28,6 +28,7 @@ F0s1 = tuning.Frequency(index);
 
 F0s = log10(F0s1);
 response = reshape(repmat(F0s, 1, 20)', 1, []);
+response = response';
 
 %% Get all rates for each repetition for bassoon (one example neuron)
 
@@ -55,10 +56,8 @@ for ind = 1:num_data
 
 	% Put data into table
 	T = array2table(h_all);
-	T.response = response';
-
+	T.response = response;
 	predictors = h_all;
-	response = response';
 	for imodelrep = 1 %:5
 
 		% Train a classifier
@@ -115,16 +114,18 @@ for ind = 1:num_data
 		correctPredictions = (validationPredictions == response);
 		isMissing = isnan(response);
 		correctPredictions = correctPredictions(~isMissing);
-		validationAccuracy = sum(correctPredictions)/length(correctPredictions);
-
+		validationAccuracy(ind) = sum(correctPredictions)/length(correctPredictions);
 		C = confusionmat(validationPredictions, response);
+
+		% Get accuracy for 1-20, 21-40
+		validationAccuracy1(ind) = sum(correctPredictions(1:400))/length(correctPredictions(1:400));
+		validationAccuracy2(ind) = sum(correctPredictions(401:800))/length(correctPredictions(401:800));
 		
 	end
 
-	figure
-	confusionchart(C)
-	title(num2str(validationAccuracy*100))
-
+	% figure
+	% confusionchart(C)
+	% title(num2str(validationAccuracy*100))
 
 	% Plots
 	% figure
@@ -148,25 +149,161 @@ for ind = 1:num_data
 	% end
 	% scatter(spikes_oboe, spikereps_oboe, 'filled')
 	% 
-	% fprintf('%d/%d, %0.2f%% done!\n', ind, num_data, ind/num_data*100)
+	fprintf('%d/%d, %0.2f%% done!\n', ind, num_data, ind/num_data*100)
 end
 
-% Plot accuracy of each neuron
-figure
-tiledlayout(4, 4)
-acc2 = squeeze(max(accuracy, 3));
-for ii = 1:16
-	nexttile
-	histogram(acc2(:,ii)*100,21)
-	mean_F0 = mean(acc2(:,ii));
-	hold on
-	xline(50, 'k')
-	xline(mean_F0*100, 'r', 'LineWidth',2)
-	ylabel('# Neurons')
-	xlabel('Prediction Accuracy (%)')
-	title(['Prediction of instrument, F0=' num2str(round(bass_pitch(ind_b(ii))))])
-end
+%% Plot accuracy of each neuron
+figure('Position',[560,618,798,230])
+tiledlayout(1, 3)
 
-mean_all = mean(acc2, 'all');
+nexttile
+edges = linspace(0, 100, 30);
+histogram(validationAccuracy*100,edges)
+mean_F0 = mean(validationAccuracy);
+hold on
+xline(2.5, 'k')
+xline(mean_F0*100, 'r', 'LineWidth',2)
+ylabel('# Neurons')
+xlabel('Prediction Accuracy (%)')
+title('Prediction of F0')
+mean_all = mean(validationAccuracy, 'all');
 fprintf('Mean for all = %0.4f\n', mean_all)
+xlim([0 100])
+legend('', 'Chance', ['Mean=' num2str(round(mean_all*100)) '%'])
+
+nexttile
+histogram(validationAccuracy1*100,edges)
+mean_F0 = mean(validationAccuracy1);
+hold on
+xline(2.5, 'k')
+xline(mean_F0*100, 'r', 'LineWidth',2)
+ylabel('# Neurons')
+xlabel('Prediction Accuracy (%)')
+title('Prediction of F0, 58-174Hz')
+mean_all = mean(validationAccuracy1, 'all');
+fprintf('Mean for all = %0.4f\n', mean_all)
+xlim([0 100])
+legend('', 'Chance', ['Mean=' num2str(round(mean_all*100)) '%'])
+
+nexttile
+histogram(validationAccuracy2*100,edges)
+mean_F0 = mean(validationAccuracy2);
+hold on
+xline(2.5, 'k')
+xline(mean_F0*100, 'r', 'LineWidth',2)
+ylabel('# Neurons')
+xlabel('Prediction Accuracy (%)')
+title('Prediction of F0, 185-587Hz')
+mean_all = mean(validationAccuracy2, 'all');
+fprintf('Mean for all = %0.4f\n', mean_all)
+xlim([0 100])
+legend('', 'Chance', ['Mean=' num2str(round(mean_all*100)) '%'])
+
+
+%% Find indices of the best neurons 
+
+[temp,originalpos] = sort(validationAccuracy, 'descend' );
+n = temp(1:3);
+best_ind=originalpos(1:3);
+
+putatives = nat_data(best_ind(1)).putative;
+
+%%
+
+for ind = best_ind
+	index = sesh(ind);
+
+	% Get data
+	h_all = [];
+	for itarget = 1:40
+		spikes_bass = nat_data(index).bass_spikerate{itarget}/1000; % ms
+		spikereps_bass = nat_data(index).bass_spikerep{itarget};
+
+		% Arrange data for SVM
+		min_dis = 0.5;
+		edges = 0:min_dis:300;
+		t = 0+min_dis/2:min_dis:300-min_dis/2;
+		for irep = 1:20
+			h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
+		end
+		h_all = [h_all; h_bass];
+	end
+
+	% Put data into table
+	T = array2table(h_all);
+	T.response = response;
+	predictors = h_all;
+	for imodelrep = 1 %:5
+
+		% Train a classifier
+		% This code specifies all the classifier options and trains the classifier.
+		template = templateLinear(...
+			'Learner', 'Logistic', ...
+			'Lambda', 'auto', ...
+			'BetaTolerance', 0.0001);
+		classificationLinear = fitcecoc(...
+			predictors, ...
+			response, ...
+			'Learners', template, ...
+			'Coding', 'onevsone');
+
+		% Perform cross-validation
+		KFolds = 5;
+		cvp = cvpartition(response, 'KFold', KFolds);
+
+		% Initialize the predictions to the proper sizes
+		validationPredictions = response;
+		numObservations = size(predictors, 1);
+		numClasses = 40;
+		validationScores = NaN(numObservations, numClasses);
+		for fold = 1:KFolds
+			trainingPredictors = predictors(cvp.training(fold), :);
+			trainingResponse = response(cvp.training(fold), :);
+
+			% Train a classifier
+			% This code specifies all the classifier options and trains the classifier.
+			template = templateLinear(...
+				'Learner', 'Logistic', ...
+				'Lambda', 'auto', ...
+				'BetaTolerance', 0.0001);
+			classificationLinear = fitcecoc(...
+				trainingPredictors, ...
+				trainingResponse, ...
+				'Learners', template, ...
+				'Coding', 'onevsone');
+
+			% Create the result struct with predict function
+			classificationLinearPredictFcn = @(x) predict(classificationLinear, x);
+			validationPredictFcn = @(x) classificationLinearPredictFcn(x);
+
+			% Compute validation predictions
+			validationPredictors = predictors(cvp.test(fold), :);
+			[foldPredictions, foldScores] = validationPredictFcn(validationPredictors);
+
+			% Store predictions in the original order
+			validationPredictions (cvp.test(fold), :) = foldPredictions;
+			validationScores(cvp.test(fold), :) = foldScores;
+		end
+
+		% Compute validation accuracy
+		correctPredictions = (validationPredictions == response);
+		isMissing = isnan(response);
+		correctPredictions = correctPredictions(~isMissing);
+		accur(ind) = sum(correctPredictions)/length(correctPredictions);
+		C = confusionmat(validationPredictions, response);
+
+		% Get accuracy for 1-20, 21-40
+		accur1(ind) = sum(correctPredictions(1:400))/length(correctPredictions(1:400));
+		accur2(ind) = sum(correctPredictions(401:800))/length(correctPredictions(401:800));
+		
+	end
+
+	figure
+	confusionchart(C)
+	title(num2str(accur(ind)*100))
+end
+
+%% Plot rasters of three best
+
+
 
