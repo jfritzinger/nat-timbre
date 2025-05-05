07923c753b7de1e7fc516ea6571f728167f9967b
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-05-05 14:48:09 -0400

    Initial commit

---
+++ scripts/helper-functions/getPathsNT.m
@@ -0,0 +1,22 @@
+function [base, datapath, savepath, ppi] = getPathsNT()
+
+ppi = get(0, 'ScreenPixelsPerInch');
+set(0,'DefaultAxesFontName', 'Arial')
+
+% Set the 'base' filepath for creating all figures 
+if ismac
+	base = ['/Users/jfritzinger/Library/CloudStorage/Box-Box/02 - Code/' ...
+		'Nat-Timbre/data/'];
+	savepath = ['/Users/jfritzinger/Library/CloudStorage/Box-Box/02 - Code' ...
+		'/Nat-Timbre/figures/'];
+else
+	base = ['C:\Users\jfritzinger\Box\02 - Code\Nat-Timbre\' ...
+		'data'];
+	savepath = ['C:\Users\jfritzinger\Box\02 - Code\Nat-Timbre\' ...
+		'figures\2025-manuscript'];
+end
+
+% Basic paths for loading data and saving figures 
+%datapath = fullfile(base, 'Neural_Data');
+datapath = [];
+end
