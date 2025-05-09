4bdd88ef2268eaf4f451b526598d7b8fb7c98c27
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-05-09 15:41:34 -0400

    Added analysis plots

--- scripts/decoding-models/plot_neuron_comparison_timbre.m
+++ scripts/decoding-models/plot_neuron_comparison_timbre.m
@@ -1,8 +1,7 @@
 %% plot_neuron_comparison_timbre
 clear 
 
-[base, ~, savepath, ppi] = getPathsNT();
-
+[base, ~, ~, ~] = getPathsNT();
 
 %% 
 
@@ -12,32 +11,32 @@ filepath = fullfile(base, 'model_comparisons',filename);
 load(filepath, "neuron_rate_timbre")
 
 % Figure
-figure('Position',[560,585,1023,263])
-tiledlayout(1, 2)
+figure('Position',[560,585,1023,500])
+tiledlayout(2, 2)
 linewidth = 2;
 
 nexttile
-accuracy = [neuron_rate_timbre.accuracy]*100;
+accuracy_rate = [neuron_rate_timbre.accuracy]*100;
 CFs = [neuron_rate_timbre.CF];
 edges = linspace(0, 100, 101);
-histogram(accuracy,edges)
+histogram(accuracy_rate,edges)
 hold on
 
 % Plot chance line
 xline(50, 'k', 'LineWidth',linewidth)
 
 % Mean and median
-xline(mean(accuracy), 'r', 'LineWidth',linewidth)
-xline(median(accuracy), 'r--', 'LineWidth',linewidth)
+xline(mean(accuracy_rate), 'r', 'LineWidth',linewidth)
+xline(median(accuracy_rate), 'r--', 'LineWidth',linewidth)
 
 % Best
-scatter(max(accuracy), 0, 'filled')
+scatter(max(accuracy_rate), 0, 'filled')
 
 % Labels
 legend('', sprintf('Chance = 50%%'), ...
-	sprintf('Mean = %.2f%%', mean(accuracy)), ...
-	sprintf('Median = %.2f%%', median(accuracy)),...
-	sprintf('Best = %.2f%%', max(accuracy)))
+	sprintf('Mean = %.2f%%', mean(accuracy_rate)), ...
+	sprintf('Median = %.2f%%', median(accuracy_rate)),...
+	sprintf('Best = %.2f%%', max(accuracy_rate)))
 ylabel('# Neurons')
 xlabel('Prediction Accuracy (%)')
 title('Instrument Predictions')
@@ -46,11 +45,74 @@ grid on
 
 % Plot accuracy vs CF
 nexttile
-scatter(CFs, accuracy, 'filled')
+scatter(CFs, accuracy_rate, 'filled')
 set(gca, 'xscale', 'log')
 xlabel('CFs (Hz)')
 ylabel('Accuracy')
 
+%% 
+
+% Load in data 
+[base, datapath, savepath, ppi] = getPathsNT();
+filename = 'Neuron_Time_Timbre_All.mat';
+filepath = fullfile(base, 'model_comparisons',filename);
+load(filepath, "neuron_time_timbre")
+
+% Figure
+nexttile
+accuracy_time = [neuron_time_timbre.accuracy]*100;
+CFs = [neuron_time_timbre.CF];
+edges = linspace(0, 100, 101);
+histogram(accuracy_time,edges)
+hold on
+
+% Plot chance line
+xline(50, 'k', 'LineWidth',linewidth)
+
+% Mean and median
+xline(mean(accuracy_time), 'r', 'LineWidth',linewidth)
+xline(median(accuracy_time), 'r--', 'LineWidth',linewidth)
+
+% Best
+scatter(max(accuracy_time), 0, 'filled')
+
+% Labels
+legend('', sprintf('Chance = 50%%'), ...
+	sprintf('Mean = %.2f%%', mean(accuracy_time)), ...
+	sprintf('Median = %.2f%%', median(accuracy_time)),...
+	sprintf('Best = %.2f%%', max(accuracy_time)))
+ylabel('# Neurons')
+xlabel('Prediction Accuracy (%)')
+title('Instrument Predictions')
+xlim([0 100])
+grid on
+
+% Plot accuracy vs CF
+nexttile
+scatter(CFs, accuracy_time, 'filled')
+set(gca, 'xscale', 'log')
+xlabel('CFs (Hz)')
+ylabel('Accuracy')
+
+%% Look at single neuron differences in predictions 
+
+figure
+hold on
+scatter(accuracy_rate, accuracy_time, 'filled', 'MarkerEdgeColor','k', ...
+	MarkerFaceAlpha=0.5)
+plot([0, 100], [0, 100], 'k')
+xlim([40 85])
+ylim([40 85])
+axis square
+
+mdl = fitlm(accuracy_rate, accuracy_time);
+x = linspace(0, 100, 20);
+y = mdl.Coefficients{2, 1}*x + mdl.Coefficients{1, 1};
+plot(x, y, 'r')
+legend('Neuron', 'Unity', 'y = 0.48*x+30.47, p=0.0000')
+xlabel('Accuracy of Rate Prediction (%)')
+ylabel('Accuracy of Timing Prediction (%)')
+title('Rate vs Timing Comparison')
 
 %% FUNCTIONS 
 
