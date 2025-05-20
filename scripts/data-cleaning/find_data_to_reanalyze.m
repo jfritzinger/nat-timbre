1d2393e883ed96fb38bb057d4303dd97fbee6d56
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-05-20 16:13:16 -0400

    Fixed bugs, uncommented some lines

--- scripts/data-cleaning/find_data_to_reanalyze.m
+++ scripts/data-cleaning/find_data_to_reanalyze.m
@@ -61,46 +61,37 @@ writetable(list,outputfilename)
 disp('Successfully created spreadsheet!')
 
 %% Step 3: Get sessions that need to have CF and tetrode depth added 
-% 
-% rating_ind = strcmp(sessions.Rating, 'Excellent');
-% rating2_ind = strcmp(sessions.Rating, 'Good');
-% interest_R24 = sessions.Rabbit==24 & sessions.Session>= 365;
-% interest_R25 = sessions.Rabbit==25 & sessions.Session>=356 & sessions.Session<631;
-% interest_rabbits = sessions.Rabbit==29 | sessions.Rabbit==27;
-% stimind = sessions.SPEC_slide_Spectral_Centroid == 1;
-% stim2ind = sessions.Natural_Timbre == 1;
-% 
-% index = (rating_ind|rating2_ind) & (stimind | stim2ind) & (interest_R25 | interest_R24 | interest_rabbits);
-% CF_missing = isnan(sessions.CF);
-% Depth_missing = isnan(sessions.Tetrode_Depth);
-% metaind = index & (CF_missing | Depth_missing); 
-% 
-% sessions_to_reanalyze = sessions.Session(metaind);
-% rabbits_to_reanalyze = sessions.Rabbit(metaind);
-% for ii = 1:length(sessions_to_reanalyze)
-% 	name{ii} = sprintf('R0%gS%3g', rabbits_to_reanalyze(ii), sessions_to_reanalyze(ii));
-% end
-% CFs = sessions.CF(metaind);
-% depths = sessions.Tetrode_Depth(metaind);
-% 
-% 
-% list = table(name', CFs, depths);
-% 
-% outputfilename = 'Fig0_AddMetadata.xlsx';
-% writetable(list,outputfilename)
-% disp('Successfully created spreadsheet!')
+
+rating_ind = strcmp(sessions.Rating, 'Excellent');
+rating2_ind = strcmp(sessions.Rating, 'Good');
+stim2ind = sessions.Natural_Timbre == 1;
+
+index = (rating_ind|rating2_ind) & (stim2ind) & (interest_27 | interest_29);
+CF_missing = isnan(sessions.CF);
+Depth_missing = isnan(sessions.Tetrode_Depth);
+metaind = index & (CF_missing | Depth_missing); 
+
+sessions_to_reanalyze = sessions.Session(metaind);
+rabbits_to_reanalyze = sessions.Rabbit(metaind);
+for ii = 1:length(sessions_to_reanalyze)
+	name{ii} = sprintf('R0%gS%3g', rabbits_to_reanalyze(ii), sessions_to_reanalyze(ii));
+end
+CFs = sessions.CF(metaind);
+depths = sessions.Tetrode_Depth(metaind);
+
+
+list = table(name', CFs, depths);
+
+outputfilename = 'AddMetadata.xlsx';
+writetable(list,outputfilename)
+disp('Successfully created spreadsheet!')
 
 %% Neuron Counts 
 
 rating_ind = strcmp(sessions.Rating, 'Excellent');
 rating2_ind = strcmp(sessions.Rating, 'Good');
-stimind = sessions.SPEC_slide_Spectral_Centroid == 1;
 stim2ind = sessions.Natural_Timbre == 1;
-LMAind = sessions.Rabbit==25 & sessions.Session>=646;
 notdriven = sessions.CF==0;
 
-index = stimind & ~LMAind & (rating_ind|rating2_ind) & ~notdriven;
-sessions_synthtimbre = sessions(index,:);
-
-index2 = stim2ind & ~LMAind & (rating_ind|rating2_ind) & ~notdriven;
-sessions_nattimbre= sessions(index2,:);
+index = (rating_ind|rating2_ind) & (stim2ind) & (interest_27 | interest_29) & ~notdriven;
+sessions_nattimbre = sessions(index,:);
