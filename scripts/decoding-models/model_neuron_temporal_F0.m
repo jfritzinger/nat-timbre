f7f65254e73035611e9d0a7680899572a181cf68
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-04 16:02:54 -0400

    Idk

--- scripts/decoding-models/model_neuron_temporal_F0.m
+++ scripts/decoding-models/model_neuron_temporal_F0.m
@@ -59,58 +59,10 @@ for ind = 1:num_data
 	% Put data into table
 	T = array2table(h_all);
 	T.response = response;
-	predictors = h_all;
+
 	for imodelrep = 1 %:5
 
-		% Train a classifier
-		% This code specifies all the classifier options and trains the classifier.
-		template = templateLinear(...
-			'Learner', 'Logistic', ...
-			'Lambda', 'auto', ...
-			'BetaTolerance', 0.0001);
-		classificationLinear = fitcecoc(...
-			predictors, ...
-			response, ...
-			'Learners', template, ...
-			'Coding', 'onevsone');
-
-		% Perform cross-validation
-		KFolds = 5;
-		cvp = cvpartition(response, 'KFold', KFolds);
-
-		% Initialize the predictions to the proper sizes
-		validationPredictions = response;
-		numObservations = size(predictors, 1);
-		numClasses = length(F0s);
-		validationScores = NaN(numObservations, numClasses);
-		for fold = 1:KFolds
-			trainingPredictors = predictors(cvp.training(fold), :);
-			trainingResponse = response(cvp.training(fold), :);
-
-			% Train a classifier
-			% This code specifies all the classifier options and trains the classifier.
-			template = templateLinear(...
-				'Learner', 'Logistic', ...
-				'Lambda', 'auto', ...
-				'BetaTolerance', 0.0001);
-			classificationLinear = fitcecoc(...
-				trainingPredictors, ...
-				trainingResponse, ...
-				'Learners', template, ...
-				'Coding', 'onevsone');
-
-			% Create the result struct with predict function
-			classificationLinearPredictFcn = @(x) predict(classificationLinear, x);
-			validationPredictFcn = @(x) classificationLinearPredictFcn(x);
-
-			% Compute validation predictions
-			validationPredictors = predictors(cvp.test(fold), :);
-			[foldPredictions, foldScores] = validationPredictFcn(validationPredictors);
-
-			% Store predictions in the original order
-			validationPredictions(cvp.test(fold), :) = foldPredictions;
-			validationScores(cvp.test(fold), :) = foldScores;
-		end
+		[validationPredictions] = trainClassifierNeuronTimeF0(T);
 
 		% Compute validation accuracy
 		correctPredictions = (validationPredictions == response);
