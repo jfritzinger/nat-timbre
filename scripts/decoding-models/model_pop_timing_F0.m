48c0f54156f906630e5523a912600014deb5046a
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-05 16:16:40 -0400

    Finished code

--- scripts/decoding-models/model_pop_timing_F0.m
+++ scripts/decoding-models/model_pop_timing_F0.m
@@ -1,12 +1,14 @@
 clear
 
 %% Load in data
+target = 'Bassoon';
 
-filepath = '/Users/jfritzinger/Library/CloudStorage/Box-Box/02 - Code/Nat-Timbre/data/model_comparisons';
-load(fullfile(filepath, 'Data_NT2.mat'), 'nat_data')
+base = getPathsNT();
+load(fullfile(base, 'model_comparisons', 'Data_NT_3.mat'), 'nat_data')
+load(fullfile(base, 'model_comparisons', ['Neuron_Time_F0_' target '.mat']),...
+	"neuron_time_F0")
 
 %% Get correct output of model
-target = 'Oboe';
 
 if ismac
 	fpath = '/Users/jfritzinger/Library/CloudStorage/Box-Box/02 - Code/Nat-Timbre/data';
@@ -22,106 +24,116 @@ note_names = extractBetween(files, 'ff.', '.');
 [~, index] = ismember(note_names, tuning.Note);
 F0s1 = tuning.Frequency(index);
 [F0s, order] = sort(F0s1);
-F0s = log10(F0s);
+
+% Find all rows with bassoon and oboe
+if strcmp(target, 'Bassoon')
+	sesh_all = find(~cellfun(@isempty, {nat_data.bass_rate}));
+else
+	sesh_all = find(~cellfun(@isempty, {nat_data.oboe_rate}));
+end
+num_data_all = numel(sesh_all);
+CFs_all = [nat_data(sesh_all).CF];
+putative = {nat_data(sesh_all).putative};
+MTFs = {nat_data(sesh_all).MTF};
+
+
+% Get indices of interest
+accuracy = [neuron_time_F0.accuracy];
+[~,originalpos] = sort(abs(accuracy), 'descend' );
+best_ind = originalpos(1:100);
+[~,originalpos] = sort(abs(accuracy), 'ascend' );
+worst_ind = originalpos(1:100);
+
+%poolobj = parpool();
+% figure
+% bar(accuracy(originalpos))
 
 %% Get data
 
 h_all2 = [];
-for ineuron = 1:3
-	if ineuron == 1
-		%putative = 'R29_TT2_P3_N03'; % Predictions 44, yesterday 62?
-		putative = 'R29_TT3_P2_N13';
-	elseif ineuron == 2
-		%putative = 'R29_TT3_P5_N05'; % Not predicting well, not sure why
-		putative = 'R29_TT1_P2_N17';
-	else
-		%putative = 'R29_TT3_P2_N06'; % Good
-		putative = 'R29_TT1_P3_N04';
+num_neurons = [1:4 5:5:50 1:4 5:5:50];
+nmodels = length(num_neurons);
+timerVal = tic;
+for imodel = 1:nmodels
+	timerVal = tic;
+
+
+	if imodel < nmodels/2+1 % 6 good models
+		index = best_ind;
+	else % 6 bad models
+		index = worst_ind;
 	end
-	index = find(strcmp({nat_data.putative}, putative));
+	num_index = 1:num_neurons(imodel);
+	num_data = num_neurons(imodel);
+	sesh = sesh_all(index(num_index));
 
-	h_all = [];
-	for itarget = 1:length(F0s)
-		if strcmp(target, 'Bassoon')
-			spikes_bass = nat_data(index).bass_spikerate{itarget}/1000; % ms
-			spikereps_bass = nat_data(index).bass_spikerep{itarget};
-		else
-			spikes_bass = nat_data(index).oboe_spikerate{itarget}/1000; % ms
-			spikereps_bass = nat_data(index).oboe_spikerep{itarget};
-		end
+	
+	for ineuron = 1:num_data
+		h_all = [];
+		for itarget = 1:length(F0s)
+			if strcmp(target, 'Bassoon')
+				spikes_bass = nat_data(sesh(ineuron)).bass_spikerate{itarget}/1000; % ms
+				spikereps_bass = nat_data(sesh(ineuron)).bass_spikerep{itarget};
+			else
+				spikes_bass = nat_data(sesh(ineuron)).oboe_spikerate{itarget}/1000; % ms
+				spikereps_bass = nat_data(sesh(ineuron)).oboe_spikerep{itarget};
+			end
 
-		% Arrange data for SVM
-		min_dis = 1;
-		edges = 0:min_dis:300;
-		t = 0+min_dis/2:min_dis:300-min_dis/2;
-		for irep = 1:20
-			h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
+			% Arrange data for SVM
+			min_dis = 1;
+			edges = 0:min_dis:300;
+			t = 0+min_dis/2:min_dis:300-min_dis/2;
+			for irep = 1:20
+				h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
+			end
+			h_all = [h_all; h_bass];
 		end
-		h_all = [h_all; h_bass];
+		h_all2 = [h_all2, h_all];
 	end
 
-	h_all2 = [h_all2, h_all];
+	% Put data into table
+	response = reshape(repmat(F0s, 1, 20)', 1, []);
+	T = array2table(h_all2);
+	T.response = response';
+	predictors = h_all2;
 
+	% Call model (classification)
+	[trainedClassifier, validationAccuracy, validationPredictions] ...
+		= trainClassifierPopTimeF0(T, F0s);
+
+	% Plot
+	figure
+	C = confusionmat(T.response, validationPredictions);
+	confusionchart(C)
+	title([target ', Accuracy = ' num2str(round(validationAccuracy*100)) '%'])
+
+	% Calculate accuracy
+	accuracy = sum(diag(C)) / sum(C(:)); % Calculate accuracy
+	title(sprintf('%d neurons, Accuracy = %0.2f%%', num_data, accuracy*100))
+	accur_all(imodel) = accuracy;
+
+	timer = toc(timerVal);
+	fprintf('Models took %0.2g minutes\n', timer/60)
+	fprintf('%d/%d, %0.2f%% done!\n', imodel, nmodels, imodel/nmodels*100)	
 end
 
-% Put data into table
-response = reshape(repmat(F0s, 1, 20)', 1, []);
-T = array2table(h_all2);
-T.response = response';
-predictors = h_all2;
+%% 
 
-
-%% Call model (classification)
-
-% [trainedClassifier, validationAccuracy, validationPredictions] ...
-% 	= trainClassifierPopTimeF0(T, F0s);
-% 
-% % Plot 
-% figure
-% C = confusionmat(T.response, validationPredictions);
-% confusionchart(C)
-% title([target ', Accuracy = ' num2str(round(validationAccuracy*100)) '%'])
-% 
-
-
-%% Call model (linear)
-
-[trainedModel, validationRMSE, validationPredictions] = trainRegressionModelPopTimeF0(T);
-
-r = corrcoef(validationPredictions, T.response);
-r2 = r(1, 2)^2;
-
-figure('Position',[31,910,910,413])
-nexttile
-scatter(T.response, validationPredictions, 'filled', 'MarkerFaceAlpha',0.5)
-%set(gca, 'xscale', 'log', 'yscale', 'log')
-% ylim([57 588])
-% xlim([57 588])
+figure
+plot(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2));
 hold on
-plot(T.response, T.response)
-title(['R^2 = ' num2str(r2)])
-ylabel('Predicted log10(F0)')
-xlabel('Actual log10(F0)')
+plot(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end))
+xlabel('Number of Neurons in Model')
+ylabel('Accuracy')
+legend('Best', 'Worst')
 
-logF0 = log10(F0s1);
-for ii = 1:length(validationPredictions)
-
-	differences = abs(logF0 - validationPredictions(ii));
-	[~, closest_column_index] = min(differences);
-	closest_cat(ii) = logF0(closest_column_index);
-end
-
-
-% Plot confusion matrix
-nexttile
-C = confusionmat(T.response, closest_cat);
-confusionchart(C)
-
-% Calculate accuracy
-chart = confusionchart(T.response,closest_cat); % Generate confusion chart
-confusionMatrix = chart.NormalizedValues; % Get the normalized confusion matrix
-accuracy = sum(diag(confusionMatrix)) / sum(confusionMatrix(:)); % Calculate accuracy
-title(sprintf('Accuracy = %0.2f%%', accuracy*100))
-
-
-
+% mean_acc = mean(accuracies,2);
+% std_acc = std(accuracies, [], 2);
+%
+% figure
+% errorbar(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2), std_acc(1:nmodels/2)/sqrt(10));
+% hold on
+% errorbar(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end), std_acc(nmodels/2+1:end)/sqrt(10))
+% xlabel('Number of Neurons in Model')
+% ylabel('Accuracy')
+% legend('Best', 'Worst')
