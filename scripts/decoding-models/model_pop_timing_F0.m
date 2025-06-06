53dd10bcff464d3e04bb02bea8287755587dc79c
Johanna Fritzinger <johanna_fritzinger@urmc.rochester.edu>
2025-06-05 21:25:32 -0400

    Fixed bug

--- scripts/decoding-models/model_pop_timing_F0.m
+++ scripts/decoding-models/model_pop_timing_F0.m
@@ -57,83 +57,91 @@ timerVal = tic;
 for imodel = 1:nmodels
 	timerVal = tic;
 
+	for irep = 1:10
 
-	if imodel < nmodels/2+1 % 6 good models
-		index = best_ind;
-	else % 6 bad models
-		index = worst_ind;
-	end
-	num_index = 1:num_neurons(imodel);
-	num_data = num_neurons(imodel);
-	sesh = sesh_all(index(num_index));
-
-	
-	for ineuron = 1:num_data
-		h_all = [];
-		for itarget = 1:length(F0s)
-			if strcmp(target, 'Bassoon')
-				spikes_bass = nat_data(sesh(ineuron)).bass_spikerate{itarget}/1000; % ms
-				spikereps_bass = nat_data(sesh(ineuron)).bass_spikerep{itarget};
-			else
-				spikes_bass = nat_data(sesh(ineuron)).oboe_spikerate{itarget}/1000; % ms
-				spikereps_bass = nat_data(sesh(ineuron)).oboe_spikerep{itarget};
-			end
-
-			% Arrange data for SVM
-			min_dis = 1;
-			edges = 0:min_dis:300;
-			t = 0+min_dis/2:min_dis:300-min_dis/2;
-			for irep = 1:20
-				h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
-			end
-			h_all = [h_all; h_bass];
+		if imodel < nmodels/2+1 % 6 good models
+			index = best_ind;
+		else % 6 bad models
+			index = worst_ind;
 		end
-		h_all2 = [h_all2, h_all];
+		num_index = 1:num_neurons(imodel);
+		num_data = num_neurons(imodel);
+		sesh = sesh_all(index(num_index));
+
+		h_all2 = [];
+		for ineuron = 1:num_data
+			h_all = [];
+			for itarget = 1:length(F0s)
+				if strcmp(target, 'Bassoon')
+					spikes_bass = nat_data(sesh(ineuron)).bass_spikerate{itarget}/1000; % ms
+					spikereps_bass = nat_data(sesh(ineuron)).bass_spikerep{itarget};
+				else
+					spikes_bass = nat_data(sesh(ineuron)).oboe_spikerate{itarget}/1000; % ms
+					spikereps_bass = nat_data(sesh(ineuron)).oboe_spikerep{itarget};
+				end
+
+				% Arrange data for SVM
+				min_dis = 1;
+				edges = 0:min_dis:300;
+				t = 0+min_dis/2:min_dis:300-min_dis/2;
+				for irep = 1:20
+					h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
+				end
+				h_all = [h_all; h_bass];
+			end
+			h_all2 = [h_all2, h_all];
+		end
+
+		% Put data into table
+		response = reshape(repmat(F0s, 1, 20)', 1, []);
+		T = array2table(h_all2);
+		T.response = response';
+		predictors = h_all2;
+
+		% Call model (classification)
+		[trainedClassifier, validationAccuracy, validationPredictions] ...
+			= trainClassifierPopTimeF0(T, F0s);
+
+		% Plot
+		%figure
+		C = confusionmat(T.response, validationPredictions);
+		%confusionchart(C)
+		%title([target ', Accuracy = ' num2str(round(validationAccuracy*100)) '%'])
+
+		% Calculate accuracy
+		accuracy = sum(diag(C)) / sum(C(:)); % Calculate accuracy
+		title(sprintf('%d neurons, Accuracy = %0.2f%%', num_data, accuracy*100))
+		accur_all(imodel, irep) = accuracy;
+		C_all{imodel, irep} = C;
 	end
-
-	% Put data into table
-	response = reshape(repmat(F0s, 1, 20)', 1, []);
-	T = array2table(h_all2);
-	T.response = response';
-	predictors = h_all2;
-
-	% Call model (classification)
-	[trainedClassifier, validationAccuracy, validationPredictions] ...
-		= trainClassifierPopTimeF0(T, F0s);
-
-	% Plot
-	figure
-	C = confusionmat(T.response, validationPredictions);
-	confusionchart(C)
-	title([target ', Accuracy = ' num2str(round(validationAccuracy*100)) '%'])
-
-	% Calculate accuracy
-	accuracy = sum(diag(C)) / sum(C(:)); % Calculate accuracy
-	title(sprintf('%d neurons, Accuracy = %0.2f%%', num_data, accuracy*100))
-	accur_all(imodel) = accuracy;
-
 	timer = toc(timerVal);
 	fprintf('Models took %0.2g minutes\n', timer/60)
-	fprintf('%d/%d, %0.2f%% done!\n', imodel, nmodels, imodel/nmodels*100)	
+	fprintf('%d/%d, %0.2f%% done!\n', imodel, nmodels, imodel/nmodels*100)
 end
 
-%% 
+%%
+
+% figure
+% plot(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2));
+% hold on
+% plot(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end))
+% xlabel('Number of Neurons in Model')
+% ylabel('Accuracy')
+% legend('Best', 'Worst')
+
+mean_acc = mean(accur_all,2);
+std_acc = std(accur_all, [], 2);
 
 figure
-plot(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2));
+errorbar(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2), std_acc(1:nmodels/2)/sqrt(10));
 hold on
-plot(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end))
+errorbar(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end), std_acc(nmodels/2+1:end)/sqrt(10))
 xlabel('Number of Neurons in Model')
 ylabel('Accuracy')
 legend('Best', 'Worst')
 
-% mean_acc = mean(accuracies,2);
-% std_acc = std(accuracies, [], 2);
-%
-% figure
-% errorbar(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2), std_acc(1:nmodels/2)/sqrt(10));
-% hold on
-% errorbar(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end), std_acc(nmodels/2+1:end)/sqrt(10))
-% xlabel('Number of Neurons in Model')
-% ylabel('Accuracy')
-% legend('Best', 'Worst')
+%% Save data
+
+save(fullfile(base, 'model_comparisons', 'pop_timing_F0_bassoon_subset.mat'), ...
+	"accur_all, C_all", "num_neurons", "mean_acc", "std_acc")
+
