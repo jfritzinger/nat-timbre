ae6bcce60482e32617bdce003119a2ff728cd5f8
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-16 10:46:00 -0400

    Initial commit

---
+++ scripts/decoding-models/functions/getTimbreSessions.m
@@ -0,0 +1,15 @@
+function [sesh, num_data] = getTimbreSessions(nat_data)
+
+%% Find all rows with bassoon and oboe in them
+
+sesh = [];
+for ii = 1:length(nat_data)
+	rate = nat_data(ii).bass_rate;
+	rate2 = nat_data(ii).oboe_rate;
+	if ~isempty(rate) && ~isempty(rate2)
+		sesh = [sesh ii];
+	end
+end
+num_data = length(sesh);
+
+end
