d070fc88a4b622c61db73ef5800c948096c0b30a
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-23 21:41:42 -0400

    Changed normalized rate to driven rate

--- scripts/decoding-models/save_data_for_decoders.m
+++ scripts/decoding-models/save_data_for_decoders.m
@@ -75,21 +75,50 @@ for ii = 1:num_sesh
 	nat_data(ii).CF = CF;
 	nat_data(ii).MTF = MTF_shape;
 
-	if ~isempty(data_oboe.rate)
-		temporal_oboe = analyzeNT_Temporal(data_oboe, CF);
+	if ~isempty(data_oboe.rate) && ~isempty(data_bass.rate)
 
-		% Get normalized rates 
-		sub_rate = data_oboe.rate-spont;
-		norm_rate = sub_rate / max(sub_rate);
+		% Subtract spont  
+		sub_rate_oboe = data_oboe.rate-spont;
+		sub_rate_bass = data_bass.rate-spont;
+
+		% Normalize rate to maximum 
+		sub_rate_all = [sub_rate_oboe; sub_rate_bass];
+		norm_rate = sub_rate_all / max(sub_rate_all);
+		norm_rate_oboe = norm_rate(1:35);
+		norm_rate_bass = norm_rate(36:end);
+
+		% Get normalized rates per rep
+		sub_rate_oboe = data_oboe.raw_rates-spont;
+		sub_rate_bass = data_bass.raw_rates-spont;
+		sub_rates = [sub_rate_oboe sub_rate_bass];
+		max_rate = max(max(sub_rates));
+		norm_rates = sub_rates / max_rate;
+		norm_rates_oboe = norm_rates(:, 1:35);
+		norm_rates_bass = norm_rates(:, 36:end);
+
+	elseif  ~isempty(data_oboe.rate)
+		sub_rate_oboe = data_oboe.rate-spont;
+		norm_rate = sub_rate_oboe / max(sub_rate_oboe);
+		norm_rate_oboe = norm_rate;
 
 		% Get normalized rates per rep
 		sub_rates = data_oboe.raw_rates-spont;
 		max_rate = max(max(sub_rates));
-		norm_rates = sub_rates / max_rate;
-		% OR 
-		% max_rate = max(sub_rates, [], 2);
-		% norm_rates = sub_rates ./ max_rate;
+		norm_rates_oboe = sub_rates / max_rate;
 
+	elseif ~isempty(data_bass.rate)
+		sub_rate_bass = data_bass.rate-spont;
+		norm_rate = sub_rate_bass / max(sub_rate_bass);
+		norm_rate_bass = norm_rate;
+
+		% Get normalized rates per rep
+		sub_rates = data_bass.raw_rates-spont;
+		max_rate = max(max(sub_rates));
+		norm_rates_bass = sub_rates / max_rate;
+	end
+
+	if ~isempty(data_oboe.rate)
+		temporal_oboe = analyzeNT_Temporal(data_oboe, CF);
 		temporal_oboe.VS_rep(temporal_oboe.VS_rep>=0.99) = NaN;
 		temporal_oboe.VS_rep(isnan(temporal_oboe.VS_rep)) = 0.0001;
 		sync_rep = temporal_oboe.VS_rep' .* data_oboe.raw_rates;
@@ -99,8 +128,8 @@ for ii = 1:num_sesh
 		nat_data(ii).oboe_rate_std = data_oboe.rate_std;
 		nat_data(ii).oboe_raterep = data_oboe.raw_rates;
 		nat_data(ii).oboe_sync_rep = sync_rep;
-		nat_data(ii).oboe_norm_rate = norm_rate; 
-		nat_data(ii).oboe_norm_rep = norm_rates;
+		nat_data(ii).oboe_norm_rate = norm_rate_oboe; 
+		nat_data(ii).oboe_norm_rep = norm_rates_oboe;
 		nat_data(ii).oboe_VS = temporal_oboe.VS;
 		nat_data(ii).oboe_VSrep = temporal_oboe.VS_rep;
 		nat_data(ii).oboe_VS_CF = temporal_oboe.VS_CF;
@@ -110,19 +139,6 @@ for ii = 1:num_sesh
 
 	if ~isempty(data_bass.rate)
 		temporal_bass = analyzeNT_Temporal(data_bass, CF);
-
-		% Get normalized rates 
-		sub_rate = data_bass.rate-spont;
-		norm_rate = sub_rate / max(sub_rate);
-
-		% Get normalized rates per rep
-		sub_rates = data_bass.raw_rates-spont;
-		max_rate = max(max(sub_rates));
-		norm_rates = sub_rates / max_rate;
-		% OR 
-		% max_rate = max(sub_rates, [], 2);
-		% norm_rates = sub_rates ./ max_rate;
-
 		temporal_bass.VS_rep(temporal_bass.VS_rep>=0.99) = NaN;
 		temporal_bass.VS_rep(isnan(temporal_bass.VS_rep)) = 0.0001;
 		sync_rep = temporal_bass.VS_rep' .* data_bass.raw_rates;
@@ -132,8 +148,8 @@ for ii = 1:num_sesh
 		nat_data(ii).bass_rate_std = data_bass.rate_std;
 		nat_data(ii).bass_raterep = data_bass.raw_rates;
 		nat_data(ii).bass_sync_rep = sync_rep;
-		nat_data(ii).bass_norm_rate = norm_rate;
-		nat_data(ii).bass_norm_rep = norm_rates;
+		nat_data(ii).bass_norm_rate = norm_rate_bass;
+		nat_data(ii).bass_norm_rep = norm_rates_bass;
 		nat_data(ii).bass_VS = temporal_bass.VS;
 		nat_data(ii).bass_VSrep = temporal_bass.VS_rep;
 		nat_data(ii).bass_VS_CF = temporal_bass.VS_CF;
