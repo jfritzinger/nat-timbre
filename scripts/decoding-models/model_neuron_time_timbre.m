9b5d97a5cccb594a30f2ecae262099b1b80d189a
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-23 21:40:44 -0400

    Reran with different bin sizes and saved outputs

--- scripts/decoding-models/model_neuron_time_timbre.m
+++ scripts/decoding-models/model_neuron_time_timbre.m
@@ -4,7 +4,7 @@ clear
 
 %% Load in data
 
-[base, datapath, savepath, ppi] = getPathsNT();
+[base, ~, ~, ~] = getPathsNT();
 load(fullfile(base,'model_comparisons', 'Data_NT_3.mat'), 'nat_data')
 
 %% Get list of all timbre stimuli (bassoon)
@@ -66,12 +66,14 @@ for ind = 1:num_data
 		spikereps_oboe = nat_data(index).oboe_spikerep{ind_o(target)};
 
 		% Arrange data for SVM
-		min_dis = 0.25;
+		min_dis = 300; %0.25;
 		edges = 0:min_dis:300;
 		t = 0+min_dis/2:min_dis:300-min_dis/2;
 		for irep = 1:20
-			h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
-			h_oboe(irep, :) = histcounts(spikes_oboe(spikereps_oboe==irep), edges);
+			h_bass1 = histcounts(spikes_bass(spikereps_bass==irep), edges);
+			h_bass(irep, :) = h_bass1(randperm(length(h_bass1)));
+			h_oboe1 = histcounts(spikes_oboe(spikereps_oboe==irep), edges);
+			h_oboe(irep, :) = h_oboe1(randperm(length(h_bass1)));
 		end
 		h_all = [h_bass; h_oboe];
 		h = [h; h_all];
@@ -97,37 +99,37 @@ for ind = 1:num_data
 	neuron_time_timbre(ind).accuracy = accuracy_all(ind);
 	neuron_time_timbre(ind).C = C;
 
-	h = [];
-	clear h_bass h_oboe
-	for target = 1:16
-
-		% Get data
-		spikes_bass = nat_data(index).bass_spikerate{ind_b(target)}/1000; % ms
-		spikereps_bass = nat_data(index).bass_spikerep{ind_b(target)};
-		spikes_oboe = nat_data(index).oboe_spikerate{ind_o(target)}/1000;
-		spikereps_oboe = nat_data(index).oboe_spikerep{ind_o(target)};
-
-		% Arrange data for SVM
-		min_dis = 5;
-		edges = 0:min_dis:300;
-		t = 0+min_dis/2:min_dis:300-min_dis/2;
-		for irep = 1:20
-			h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
-			h_oboe(irep, :) = histcounts(spikes_oboe(spikereps_oboe==irep), edges);
-		end
-		h_all = [h_bass; h_oboe];
-		h = [h; h_all];
-	end
-
-	% Put data into table
-	T = array2table(h);
-	T.Instrument = repmat([ones(20,1); ones(20, 1)*2], 16, 1);
-
-	% Call SVM
-	[trainedClassifier, validationAccuracy, validationPredictions] = ...
-		trainClassifierTimeTimbre(T);
-	C = confusionmat(validationPredictions, T.Instrument);
-	accuracy_60(ind) = sum(diag(C)) / sum(C, "all");
+	% h = [];
+	% clear h_bass h_oboe
+	% for target = 1:16
+	% 
+	% 	% Get data
+	% 	spikes_bass = nat_data(index).bass_spikerate{ind_b(target)}/1000; % ms
+	% 	spikereps_bass = nat_data(index).bass_spikerep{ind_b(target)};
+	% 	spikes_oboe = nat_data(index).oboe_spikerate{ind_o(target)}/1000;
+	% 	spikereps_oboe = nat_data(index).oboe_spikerep{ind_o(target)};
+	% 
+	% 	% Arrange data for SVM
+	% 	min_dis = 5;
+	% 	edges = 0:min_dis:300;
+	% 	t = 0+min_dis/2:min_dis:300-min_dis/2;
+	% 	for irep = 1:20
+	% 		h_bass(irep, :) = histcounts(spikes_bass(spikereps_bass==irep), edges);
+	% 		h_oboe(irep, :) = histcounts(spikes_oboe(spikereps_oboe==irep), edges);
+	% 	end
+	% 	h_all = [h_bass; h_oboe];
+	% 	h = [h; h_all];
+	% end
+	% 
+	% % Put data into table
+	% T = array2table(h);
+	% T.Instrument = repmat([ones(20,1); ones(20, 1)*2], 16, 1);
+	% 
+	% % Call SVM
+	% [trainedClassifier, validationAccuracy, validationPredictions] = ...
+	% 	trainClassifierTimeTimbre(T);
+	% C = confusionmat(validationPredictions, T.Instrument);
+	% accuracy_60(ind) = sum(diag(C)) / sum(C, "all");
 
 	% Plots
 	% figure
@@ -155,11 +157,10 @@ for ind = 1:num_data
 		num_data, ind/num_data*100, accuracy_all(ind)*100)
 end
 
-% save(fullfile(base, 'model_comparisons', 'Neuron_Time_Timbre_All.mat'), ...
-% 	"neuron_time_timbre", '-v7.3')
+save(fullfile(base, 'model_comparisons', 'Neuron_Time_Timbre_All_Coarse300.mat'), ...
+	"neuron_time_timbre", '-v7.3')
 
 %% Plot accuracy of each neuron
-
 
 figure
 histogram(accuracy_all*100,21)
@@ -175,6 +176,24 @@ mean_all = mean(accuracy_all, 'all');
 fprintf('Mean for all = %0.4f\n', mean_all)
 
 %save('Neuron_Time_Timbre_All.mat', "accuracy", "mean_F0")
+
+%% Plot scatter plot 
+
+[base, datapath, savepath, ppi] = getPathsNT();
+load(fullfile(base, 'model_comparisons', 'Neuron_Rate_Timbre_All.mat'), ...
+	"neuron_rate_timbre")
+accuracy_rate = [neuron_rate_timbre.accuracy];
+
+figure
+scatter(accuracy_rate, accuracy_all, 'filled', 'MarkerEdgeColor','k');
+hold on
+xlim([0.4 1])
+ylim([0.4 1])
+plot([0.4 1], [0.4 1], 'k')
+ylabel('')
+xlabel('Rate Accuracy')
+ylabel('Timing Accuracy')
+title('Model using shuffled              PSTH')
 
 %% Get all rates for each repetition for bassoon (one example neuron)
 %
