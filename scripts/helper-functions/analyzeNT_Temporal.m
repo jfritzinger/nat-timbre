279f5dd1d93073b45f6ec53849a4e176e990625b
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-06-27 08:56:44 -0400

    Added reliability metric

--- scripts/helper-functions/analyzeNT_Temporal.m
+++ scripts/helper-functions/analyzeNT_Temporal.m
@@ -19,10 +19,11 @@ for i_stim = 1:num_stim
 	% Cut off onset
 	freq = data_ST.pitch_num(i_stim);
 	period = 1000 / freq; % Period in ms
-	num_bins = 30; % Number of bins for histogram
+	num_bins = 40; % Number of bins for histogram
 	wrapped_times = mod(x/1000, period); % Wrap spike times to one period
 	edges = linspace(0, period, num_bins+1); % Bin edges
 	counts = histcounts(wrapped_times, edges); % Create histogram
+	%phases = 2 * pi * mod(x/1000, period) / period;
 
 	% Normalize counts to get firing rate (spikes/sec)
 	% bin_width = period / num_bins; % Bin width in ms
@@ -36,7 +37,6 @@ for i_stim = 1:num_stim
 	spike_times(spike_times<50) = [];
 
 	% Calculate vector strength
-	period = 1000 / freq;
 	phases = 2 * pi * mod(spike_times, period) / period;
 	VS = abs(mean(exp(1i * phases)));
 	%vectors = exp(-1i*2*pi*200*spike_times/1000); % same equation
@@ -62,6 +62,31 @@ for i_stim = 1:num_stim
 		% histogram(raw_spikes, 301)
 	end
 
+	% Calculate reliability metric 
+	edges2 = linspace(0, 300,601);
+	rep_ind_even = mod(y,2) == 0;
+	raw_spikes_even = x(rep_ind_even)/1000;
+	[PSTH_even, ~] = histcounts(raw_spikes_even, edges2);
+
+	rep_ind_odd = mod(y,2) == 1;
+	raw_spikes_odd = x(rep_ind_odd)/1000;
+	[PSTH_odd, t] = histcounts(raw_spikes_odd, edges2);
+	r = corrcoef(PSTH_even, PSTH_odd);
+	r_splithalf(i_stim) = r(1,2);
+
+	% 	PSTHs were convolved with a Gaussian smoothing function before
+	% the correlation was computed. The SD of the Gaussian smoothing
+	% function was referred to as the temporal analysis window, which was
+	% varied over a large range (0.2, 0.6, 1.6, 4.5, and 12.8 ms) to examine
+	% both slowly and rapidly changing temporal information. The Gaussian
+	% function was truncated to 3 SD in length (as in Martin et al. 2004). For
+	% each temporal analysis window, the PSTH was convolved with the
+	% Gaussian smoothing function, and the bin width of the resulting PSTH
+	% was chosen to match the temporal analysis window.
+
+	% Correlate even and odd to get reliability 
+
+
 	% Save outputs
 	temporal.x{i_stim} = x;
 	temporal.y{i_stim} = y;
@@ -73,6 +98,7 @@ for i_stim = 1:num_stim
 	temporal.VS(i_stim) = VS;
 	temporal.VS_CF(i_stim) = VS_CF;
 	temporal.VS_rep(i_stim, :) = VS2;
+	temporal.r_splithalf(i_stim) = r_splithalf(i_stim);
 end
 
 if num_stim == 0
