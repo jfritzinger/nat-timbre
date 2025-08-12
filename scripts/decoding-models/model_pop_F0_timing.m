6391a966be79868fbb312daa1ea7951c52ed95a5
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-08-12 14:30:06 -0400

    Added 80/20 training/testing split

--- scripts/decoding-models/model_pop_F0_timing.m
+++ scripts/decoding-models/model_pop_F0_timing.m
@@ -1,26 +1,26 @@
 clear
 
 %% Load in data
-%target = 'Bassoon';
+target = 'Bassoon';
 %target = 'Oboe';
-target = 'Invariant';
+%target = 'Invariant';
 
 base = getPathsNT();
-% load(fullfile(base, 'model_comparisons', 'Data_NT_3.mat'), 'nat_data')
-% if strcmp(target, 'Invariant')
-% 	load(fullfile(base, 'model_comparisons', 'Neuron_Time_F0_Bassoon.mat'),...
-% 		"neuron_time_F0")
-% else
-% 	load(fullfile(base, 'model_comparisons', ['Neuron_Time_F0_' target '.mat']),...
-% 		"neuron_time_F0")
-% end
+load(fullfile(base, 'model_comparisons', 'Data_NT_3.mat'), 'nat_data')
+if strcmp(target, 'Invariant')
+	load(fullfile(base, 'model_comparisons', 'Neuron_Time_F0_Bassoon.mat'),...
+		"neuron_time_F0")
+else
+	load(fullfile(base, 'model_comparisons', ['Neuron_Time_F0_' target '.mat']),...
+		"neuron_time_F0")
+end
 
-load(fullfile(base, 'model_comparisons',  'Model_NT2.mat'), 'nat_model')
-nat_data = nat_model;
-load(fullfile(base, 'model_comparisons', ['Model_Neuron_Time_F0_' target '.mat']),...
-	"neuron_time_F0")
-%neuron_time_F0 = neuron_time_F0(1:183);
-neuron_time_F0 = neuron_time_F0(1:174);
+% load(fullfile(base, 'model_comparisons',  'Model_NT2.mat'), 'nat_model')
+% nat_data = nat_model;
+% load(fullfile(base, 'model_comparisons', ['Model_Neuron_Time_F0_' target '.mat']),...
+% 	"neuron_time_F0")
+% %neuron_time_F0 = neuron_time_F0(1:183);
+% neuron_time_F0 = neuron_time_F0(1:174);
 
 
 %% Get correct output of model
@@ -34,19 +34,17 @@ accuracy = [neuron_time_F0.accuracy];
 [~,best_ind] = sort(abs(accuracy), 'descend');
 [~,worst_ind] = sort(abs(accuracy), 'ascend');
 
-%poolobj = parpool();
-% figure
-% bar(accuracy(originalpos))
-
 %% Get data
 
-num_neurons = [1:4 5:5:40 40:10:100 1:4 5:5:40 40:10:100];
+%num_neurons = [1:4 5:5:40 40:10:100 1:4 5:5:40 40:10:100];
+num_neurons = [1:4 5:5:30 1:4 5:5:30];
+
 nmodels = length(num_neurons);
 timerVal = tic;
 for imodel = 1:nmodels
 	timerVal = tic;
 
-	for inrep = 1
+	for inrep = 1:2
 
 		if imodel < nmodels/2+1 % 6 good models
 			index = best_ind;
@@ -57,22 +55,21 @@ for imodel = 1:nmodels
 		num_data = num_neurons(imodel);
 		sesh = sesh_all(index(num_index));
 
-		T = getF0PopTable(nat_data, target, sesh, F0s, num_data, [], 'Timing', 'Model');
+		% Get data into table
+		T = getF0PopTable(nat_data, target, sesh, F0s, num_data, [], 'Timing', 'Data');
+
+		% Separate out training and testing data
+		[T_train, T_test] = splitData(T); 
 
 		% Call model (classification)
 		[trainedClassifier, validationAccuracy, validationPredictions] ...
-			= trainClassifierPopTimeF0(T, F0s);
+			= trainClassifierPopTimeF0(T_train, F0s);
 
-		% Plot
-		%figure
-		C = confusionmat(T.response, validationPredictions);
-		%confusionchart(C)
-		%title([target ', Accuracy = ' num2str(round(validationAccuracy*100)) '%'])
+		% To make predictions with the returned 'trainedClassifier' on new data
+		[yfit,scores] = trainedClassifier.predictFcn(T_test);
+		C = confusionmat(T_test.response, yfit);
+		accuracy(imodel, inrep) = sum(diag(C))/sum(C, 'all');
 
-		% Calculate accuracy
-		accuracy = sum(diag(C)) / sum(C(:)); % Calculate accuracy
-		accur_all(imodel, inrep) = accuracy;
-		C_all{imodel, inrep} = C;
 	end
 	timer = toc(timerVal);
 	fprintf('Models took %0.2g minutes, Accuracy = %0.1f%%\n', timer/60, accuracy*100)
@@ -81,10 +78,10 @@ end
 
 %% Plot
 
-mean_acc = mean(accur_all,2);
-std_acc = std(accur_all, [], 2);
-
 figure
+mean_acc = mean(accuracy,2);
+std_acc = std(accuracy, [], 2);
+nexttile
 errorbar(num_neurons(1:nmodels/2), mean_acc(1:nmodels/2), std_acc(1:nmodels/2)/sqrt(10));
 hold on
 errorbar(num_neurons(nmodels/2+1:end), mean_acc(nmodels/2+1:end), std_acc(nmodels/2+1:end)/sqrt(10))
@@ -96,6 +93,6 @@ legend('Best', 'Worst')
 
 % save(fullfile(base, 'model_comparisons', ['Pop_Timing_F0_' target '.mat']), ...
 % 	"accur_all","C_all", "num_neurons", "mean_acc", "std_acc")
-save(fullfile(base, 'model_comparisons', ['Model_Pop_Timing_F0_' target '.mat']), ...
- 	"accur_all","C_all", "num_neurons", "mean_acc", "std_acc")
+% save(fullfile(base, 'model_comparisons', ['Model_Pop_Timing_F0_' target '.mat']), ...
+%  	"accur_all","C_all", "num_neurons", "mean_acc", "std_acc")
 
